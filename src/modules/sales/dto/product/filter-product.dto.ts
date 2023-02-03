fa3bfb32be9c160251e2237396dcc863a927d869
basganan <bas.ganan@yavirac.edu.ec>
2023-02-03 12:50:40 -0500

    clase

--- src/modules/sales/dto/product/filter-product.dto.ts
+++ src/modules/sales/dto/product/filter-product.dto.ts
@@ -2,4 +2,8 @@ import { PaginationDto } from "./pagination.dto";
 
 export class ProductFilter extends PaginationDto {
 
+
+    @IsOptional()
+    @IsString(IsStringValidationOptions())
+    readonly title;
 }
--- src/modules/sales/dto/product/read-product.dto.ts
+++ src/modules/sales/dto/product/read-product.dto.ts
@@ -2,4 +2,11 @@ import { BaseProductDto } from "./base-product.dto";
 
 export class ReadProduct extends BaseProductDto{
     
+
+    @Expose()
+
+}
+
+function Expose() {
+    throw new Error("Function not implemented.");
 }
